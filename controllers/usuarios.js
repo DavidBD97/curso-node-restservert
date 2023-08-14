d355d89e6200b657ffe44d433f3044f6b60d4127
David Gonzalez <da_gonzalez97@hotmail.com>
2023-08-14 15:01:51 -0600

    Backend basico listo

---
+++ controllers/usuarios.js
@@ -0,0 +1,52 @@
+const {response} = require('express');
+
+
+
+const usuariosGet = (req, res = response)=>{
+    
+    const query = req.query;
+    res.json({
+        msg: 'get API - controlador',
+        query
+        
+    });
+}
+
+
+const usuariosPost = (req, res)=>{
+    const body = req.body;
+
+    res.json({
+        msg: 'post API - controlador',
+        body
+    });
+}
+
+const usuariosPut =(req, res)=>{
+    const {id} = req.params;
+    res.json({
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosPatch = (req, res)=>{
+    res.json({
+        msg: 'patch API - controlador'
+    });
+}
+
+const usuariosDelete = (req, res)=>{
+    res.json({
+        msg: 'delete API - controlador'
+    });
+}
+
+module.exports = {
+
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+}
